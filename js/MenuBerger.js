2eac843d50e1e9d8e50a8a04d1ecfc6439964e28
Ursenten19 <Zitout.Abdelhamid.at@gmail.com>
2017-02-24 21:45:05 +0100

    Commit 0

---
+++ js/MenuBerger.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function(){
+	$('.btn-navigation').click(function(){
+		$('.navigation').toggleClass('isOpen'); // toggleCalass('0'): ajoute la classe 0 à la classe navigation si navigation possede pas la classe 0 et 
+												//enleve la classe 0 à la classe navigation si navigation possede la classe 0
+	});
+	$('.btn-navigation').click(function(){
+		$('.barre').toggleClass('isWhite'); // toggleCalass('0'): ajoute la classe 0 à la classe navigation si navigation possede pas la classe 0 et 
+												//enleve la classe 0 à la classe navigation si navigation possede la classe 0
+	});
+});
